0a2d06c0ecb195cd84a8973a3ee03c4aabacd98e
swfz <sawafuji.09@gmail.com>
2018-09-29 12:11:08 +0900

    [socialcount] add star count, change interface in hatena

--- social-count/src/Code.ts
+++ social-count/src/Code.ts
@@ -1,11 +1,56 @@
-function hatena(url: string): number {
+export interface IHatenaBookmark {
+  count: number;
+  comments: string[];
+  tags: string[];
+}
+
+function hatena(url: string): IHatenaBookmark {
   const apiUrl = "http://b.hatena.ne.jp/entry/jsonlite/?url=";
   const requestUrl = `${apiUrl}${url}`;
   const res = UrlFetchApp.fetch(requestUrl);
+
+  const result = {
+    comments: [],
+    count: 0,
+    tags: [],
+  } as IHatenaBookmark;
+
   if (res != "null") {
-    return JSON.parse(res)['count'];
+    const json = JSON.parse(res);
+    result.count = json.count;
+    result.comments = json.bookmarks.map(_ => _.comment).filter(_ => _ != "");
+    result.tags = json.bookmarks.reduce((arr, cur) => arr.concat(cur.tags), []);
   }
-  return 0;
+  return result;
+}
+
+export interface IHatenaStar {
+  stars: number;
+  colored: number;
+}
+function hatenaStar(url: string): IHatenaStar {
+  const apiUrl = "http://s.hatena.com/entry.json?uri=";
+  const requestUrl = `${apiUrl}${url}`;
+  const res = UrlFetchApp.fetch(requestUrl);
+
+  const result = {
+    colored: 0,
+    stars: 0,
+  } as IHatenaStar;
+
+  if (res != "null") {
+    const entry = JSON.parse(res)["entries"][0];
+    if (entry && entry["stars"]) {
+      result.stars = entry.stars.length;
+    }
+    if (entry && entry["colored_stars"]) {
+      result.colored = entry.colored_stars.reduce((acc, cur) => {
+        const starsByColor = cur.stars.reduce((p, c) => p.count + c.count);
+        return acc + starsByColor;
+      }, 0);
+    }
+  }
+  return result;
 }
 
 function twitter(url: string): number {
